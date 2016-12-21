42db212bc0ab295dae4e0ea75fc594e511e8105d
eugen <eugen922@yahoo.de>
2016-12-21 12:25:12 +0100

    Simple Restservice, test pls

---
+++ service/rest.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by eugen on 21.12.16.
+ */
+import {Http, Headers} from '@angular/http';
+import {RequestOptions} from '@angular/http';
+import 'rxjs/add/operator/map';
+import any = jasmine.any;
+
+export class Rest {
+    static get parameters() {
+        return [{Http}];
+    }
+}
+
+function getMovie(movie_name: any, movie_date: any){
+    let url= 'http://www.omdbapi.com/?t=' + encodeURI(movie_name) + '?y=' + encodeURI(movie_date) + '&plot=short&r=jason';
+    let response: any;
+    return this.http.get(url).map(res => res.json());
+}
