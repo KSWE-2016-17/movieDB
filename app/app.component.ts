8acb9d4056fbb37ef1d9bf6616c69d12facdaff4
TwentyOneLunex <krassover@web.de>
2016-12-21 15:04:23 +0100

    verschönerungen

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,19 +7,27 @@ import { Component } from '@angular/core';
     <!-- <h1>{{title}}</h1> -->
     <nav>
       
+      <!-- 
       <a routerLink="/home" routerLinkActive="active"><img src="content/logo.png" alt="Logo / Home" /></a>
-      
-      <input (keyup.enter)="onKey($event)">
-     <!-- 
-     <p>{{suchString}}</p> 
      -->
+      <a routerLink="/home" routerLinkActive="active">Startseite</a>
       
+      Suche: <input (keyup.enter)="onKey($event)">
+      <!-- 
+      <p>{{suchString}}</p> 
+      -->
+     
+      <!--
       <a routerLink="/heroes" routerLinkActive="active">Filter (Dropdown)</a>
+      -->
       <a routerLink="/movies" routerLinkActive="active">Filme</a>
       <a routerLink="/music" routerLinkActive="active">Musik</a>
       <a routerLink="/books" routerLinkActive="active">Bücher</a>
+      
+      <!-- 
       <a routerLink="/heroes" routerLinkActive="active">Heroes (debug)</a>
       <a routerLink="/dashboard" routerLinkActive="active">Dashboard (debug)</a>
+      -->
     </nav>
     <router-outlet></router-outlet>
   `,
