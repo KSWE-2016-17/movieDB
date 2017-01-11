a840e1bc52d191b607a60fb2eb1c2e973da5f99f
TwentyOneLunex <krassover@web.de>
2017-01-11 11:50:21 +0100

    wuluwu

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { HeroDetailComponent }  from './components/hero-detail.component';
 import { HomeComponent } from "./components/home.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/movies', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'dashboard',  component: DashboardComponent },
   { path: 'home',       component: HomeComponent },
   { path: 'music',      component: MusicComponent },
